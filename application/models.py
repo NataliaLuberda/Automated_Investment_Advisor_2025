1a43bcc382ded607f5fbacc23328efe505ef5a10
nifiz <nifizek@gmail.com>
2025-04-26 15:28:21 +0200

    Model strony konta

--- application/models.py
+++ application/models.py
@@ -1,7 +1,6 @@
 from sqlalchemy import Column, Integer, String
 from application.database import Base
 
-
 class User(Base):
     __tablename__ = "users"
 
--- application/views/konto.py
+++ application/views/konto.py
@@ -1,8 +1,24 @@
 from nicegui import ui
-
+#from ..models.konto_model import Konto
 
 @ui.page('/')
 def konto_page():
+
+    #uzytkownik = Konto("Jan", "Kowalski")
+    
+    _reset_styling()
+
+    with ui.element('div').classes('w-screen h-screen flex items-center justify-center'):
+        with ui.card().classes('flex flex-col items-center justify-center'):
+            ui.label(f"Konto użytkownika {"Jan Kowalski"}").classes("text-4xl")
+            with ui.card_section():
+                with ui.row():
+                    ui.label("sigma")
+                    ui.label("sigma2")
+                    
+
+
+def _reset_styling() -> None:
     ui.add_head_html('''
         <style>
             html, body {
@@ -12,8 +28,3 @@ def konto_page():
             }
         </style>
         ''')
-    with ui.element('div').classes('w-screen h-screen flex items-center justify-center'):
-        with ui.card().classes('flex flex-col items-center justify-center'):
-            ui.label("hello")
-            with ui.card_section():
-                ui.label("something else")
