cc2417c32e9c27824e36172a910046a58dce833f
nifiz <nifizek@gmail.com>
2025-04-26 18:43:14 +0200

    Costam costam

--- application/models_/konto_model.py
+++ application/models_/konto_model.py
@@ -1,38 +1,139 @@
 import uuid
 from ..utility.waluty import Waluta
 from datetime import datetime
+from dataclasses import dataclass
+from typing import Optional
+
+class ModelStronyKonto:
+    pass
 
 class KontoUzytkownika:
-    def __init__(self, imie: str, nazwisko: str, stan_konta: float) -> 'KontoUzytkownika':
+    def __init__(self, imie: str, nazwisko: str) -> 'KontoUzytkownika':
         self._id = uuid.uuid4()
         self._imie = imie
         self._nazwisko = nazwisko
-        self._stan_konta = stan_konta
         self._rachunki_bankowe: list[RachunekBankowy] = list()
 
     def daj_id(self) -> uuid.UUID:
         return self._id
     
+    @property
+    def imie(self):
+        return self._imie
+
     def daj_imie_nazwisko(self) -> str:
         return f"{self._imie} {self._nazwisko}"
     
     def dodaj_rachunek_bankowy(self, waluta: Waluta, kwota: float = 0, ) -> None:
-        self._rachunki_bankowe.append(RachunekBankowy(kwota=kwota, rodzaj_waluty=waluta))
+        self._rachunki_bankowe.append(RachunekBankowy(self._id, kwota=kwota, rodzaj_waluty=waluta))
 
     def daj_rachunki_uzytkownika(self) -> list['RachunekBankowy']:
         return self._rachunki_bankowe
 
 class RachunekBankowy:
-    def __init__(self, kwota: float, rodzaj_waluty: Waluta):
+    def __init__(self, id_wlasciciela: uuid.UUID, kwota: float, rodzaj_waluty: Waluta):
+        self._id_wlasciciela = id_wlasciciela
         self._id = uuid.uuid4()
         self._kwota: float = kwota
         self._waluta: Waluta = rodzaj_waluty
+        self._transakcje: list[Transakcja] = list()
+    
+    @property
+    def kwota(self) -> float:
+        return self._kwota
+    
+    @property
+    def id_wlasciciela(self) -> uuid.UUID:
+        return self._id_wlasciciela
 
-    def get_kwota(self) -> float:
+    @property
+    def id(self) -> uuid.UUID:
+        return self._id
+
+# class Transakcja:
+#     def __init__(self, od: uuid.UUID, do: uuid.UUID, timestamp: datetime, kwota: float):
+#         self._id_rachunku_zrodlowego: uuid.UUID = od
+#         self._id_rachunku_adresata: uuid.UUID = do
+#         self._timestamp: datetime = timestamp
+#         self._kwota: float = kwota
+    
+#     @property
+#     def id_rachunku_zrodlowego(self) -> uuid.UUID:
+#         return self._id_rachunku_zrodlowego
+    
+#     @property
+#     def id_rachunku_adresata(self) -> uuid.UUID:
+#         return self._id_rachunku_adresata
+    
+#     @property
+#     def timestamp(self) -> datetime:
+#         return self._timestamp
+    
+#     @property
+#     def kwota(self) -> float:
+#         return self._kwota
+
+@dataclass(frozen=True)
+class Transakcja:
+    id_rachunku_zrodlowego: uuid.UUID
+    id_rachunku_adresata: uuid.UUID
+    timestamp: datetime
+    kwota: float
+
+class TransakcjaBuilder:
+    def __init__(self):
+        self._od = None
+        self._do = None
+        self._kwota = None
+        self._timestamp = None
+    
+    @property
+    def od(self) -> Optional[uuid.UUID]:
+        return self._od
+
+    @od.setter
+    def od(self, value: uuid.UUID):
+        self._od = value
+
+    @property
+    def do(self) -> Optional[uuid.UUID]:
+        return self._do
+
+    @do.setter
+    def do(self, value: uuid.UUID):
+        self._do = value
+
+    @property
+    def kwota(self) -> Optional[float]:
         return self._kwota
 
-class Transakcja:
-    def __init__(self, od: uuid.UUID, do: uuid.UUID, timestamp: datetime):
-        self._id_rachunku_zrodlowego: uuid.UUID = od
-        self._id_rachunku_adresata: uuid.UUID = do
-        self._timestamp: datetime = timestamp
+    @kwota.setter
+    def kwota(self, value: float):
+        if value <= 0:
+            raise ValueError("Kwota must be positive")
+        self._kwota = value
+
+    @property
+    def timestamp(self) -> Optional[datetime]:
+        return self._timestamp or datetime.now()
+
+    @timestamp.setter
+    def timestamp(self, value: datetime):
+        self._timestamp = value
+    
+    def build(self) -> Transakcja:
+        if not self.czy_poprawne_parametry():
+            raise ValueError("Transakcja ma błędne parametry")
+        else:
+            return Transakcja(
+                id_rachunku_zrodlowego=self._od,
+                id_rachunku_adresata=self._do,
+                kwota=self._kwota,
+                timestamp=self._timestamp
+            )
+    
+    def czy_poprawne_parametry(self) -> bool:
+        if None in (self._od, self._do, self._kwota, self._timestamp):
+            return False
+        else:
+            return True
--- application/views/konto.py
+++ application/views/konto.py
@@ -1,35 +1,41 @@
 from nicegui import ui
-from ..models_.konto_model import KontoUzytkownika
+from ..models_.konto_model import KontoUzytkownika, Transakcja, TransakcjaBuilder
 from ..utility.waluty import Waluta
 
 @ui.page('/')
 def konto_page():
-
-    uzytkownik = KontoUzytkownika("Jan", "Kowalski", stan_konta=6534.27)
-    uzytkownik.dodaj_rachunek_bankowy(Waluta.PLN, 2137.20)
+    uzytkownik = KontoUzytkownika("Jan", "Kowalski")
+    uzytkownik.dodaj_rachunek_bankowy(Waluta.PLN, kwota=2137.20)
     uzytkownik.dodaj_rachunek_bankowy(Waluta.USD, kwota=1776.0)
     
     _reset_styling()
 
     with ui.element('div').classes('w-screen h-screen flex items-center justify-center'):
-        with ui.card().classes('flex flex-col items-center justify-center'):
-            ui.label(uzytkownik.daj_imie_nazwisko()).classes("text-4xl")
-            with ui.tabs() as tabs:
-                wydatki = ui.tab("Wydatki")
-                historia = ui.tab("Historia")
-                rachunki = ui.tab("Moje rachunki")
-            with ui.tab_panels(tabs, value=wydatki):
-                with ui.tab_panel(wydatki):
-                    ui.label("Twoja historia wydatków")
-                with ui.tab_panel(historia):
-                    ui.label("Twoja historia wydatków")
-                with ui.tab_panel(rachunki):
-                    for rachunek in uzytkownik.daj_rachunki_uzytkownika():
-                        with ui.row():
-                            ui.label(f"Stan konta: {rachunek.get_kwota()} {rachunek._waluta.name}")
-                            ui.separator()
-                    
 
+        with ui.card().classes('w-96 min-h-0 max-h-full flex flex-col items-center justify-center'):
+
+            with ui.card_section().classes("w-full"):
+                ui.label(uzytkownik.daj_imie_nazwisko()).classes("text-4xl")
+                ui.separator()
+
+                with ui.row() as pole_na_nowy_przelew:
+                    ui.button("Nowy przelew", on_click=lambda: nowy_przelew_pole(pole_na_nowy_przelew))
+
+            with ui.card_section():
+                with ui.tabs() as tabs:
+                    wydatki = ui.tab("Wydatki")
+                    historia = ui.tab("Historia")
+                    rachunki = ui.tab("Moje rachunki")
+                with ui.tab_panels(tabs, value=wydatki):
+                    with ui.tab_panel(wydatki):
+                        ui.label("Twoja historia wydatków")
+                    with ui.tab_panel(historia):
+                        ui.label("Twoja historia wydatków")
+                    with ui.tab_panel(rachunki):
+                        for rachunek in uzytkownik.daj_rachunki_uzytkownika():
+                            with ui.row():
+                                ui.label(f"Stan konta: {rachunek.kwota} {rachunek._waluta.name}")
+                                ui.separator()
 
 def _reset_styling() -> None:
     ui.add_head_html('''
@@ -41,3 +47,26 @@ def _reset_styling() -> None:
             }
         </style>
         ''')
+    
+def nowy_przelew_pole(sekcja) -> None:
+
+    transakcja_szablon = TransakcjaBuilder()
+
+    sekcja.clear()
+    with ui.column():
+        ui.number(label="Kwota", value=0, min=0.01, max=999999999.99, step=1.0)
+
+        with ui.row().classes('flex align-center items-center'):
+            ui.label("Adresat:")
+            PLACEHOLDER_FOR_NOW = {1: "Jan Joński", 2: "Artur Arktyczny", 3: "Tomasz Totalitarny"}
+            czy_wziac_adresata_z_listy_kontaktow_switch = ui.switch("Z kontaktów")
+
+            adresaci_kontakty_dropdown = ui.select(PLACEHOLDER_FOR_NOW, value=1).bind_visibility_from(czy_wziac_adresata_z_listy_kontaktow_switch, 'value')
+            adresaci_numer_konta = ui.input(label="Numer rachunku").bind_visibility_from(czy_wziac_adresata_z_listy_kontaktow_switch, 
+                                                                                        'value', lambda x: not x)
+        ui.button("Wyślij", on_click=nowe_zlecenie_przelewu_wyslij())
+
+def nowe_zlecenie_przelewu_wyslij(transakcja: Transakcja) -> None:
+    # Powinna tu wlecieć transakcja z poprawnymi parametrami
+    # TODO
+    pass
