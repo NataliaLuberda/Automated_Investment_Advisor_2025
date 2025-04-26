ae0d58b968cf801fc855ee1bc70cc081dfcda92c
nifiz <nifizek@gmail.com>
2025-04-26 15:44:52 +0200

    Strona konto korzysta z modelu konta uzytkownika.

--- application/views/konto.py
+++ application/views/konto.py
@@ -1,20 +1,24 @@
 from nicegui import ui
-#from ..models.konto_model import Konto
+from ..models_.konto_model import KontoUzytkownika
 
 @ui.page('/')
 def konto_page():
 
-    #uzytkownik = Konto("Jan", "Kowalski")
+    uzytkownik = KontoUzytkownika("Jan", "Kowalski", stan_konta=6534.27)
     
     _reset_styling()
 
     with ui.element('div').classes('w-screen h-screen flex items-center justify-center'):
         with ui.card().classes('flex flex-col items-center justify-center'):
-            ui.label(f"Konto użytkownika {"Jan Kowalski"}").classes("text-4xl")
-            with ui.card_section():
-                with ui.row():
-                    ui.label("sigma")
-                    ui.label("sigma2")
+            ui.label(uzytkownik.daj_imie_nazwisko()).classes("text-4xl")
+            with ui.tabs() as tabs:
+                wydatki = ui.tab("Wydatki")
+                historia = ui.tab("Historia")
+            with ui.tab_panels(tabs, value=wydatki):
+                with ui.tab_panel(wydatki):
+                    ui.label("Twoja historia wydatków")
+                with ui.tab_panel(historia):
+                    ui.label("Twoja historia wydatków")
                     
 
 
