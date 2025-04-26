08b1cfc2e8342b1dcd2f877d1aa2922b9cbec416
nifiz <nifizek@gmail.com>
2025-04-26 14:15:49 +0200

    Usunięcie niepotrzebnego pliku

--- application/main.py
+++
@@ -1,8 +0,0 @@
-from nicegui import ui
-from application.views.login import login_page
-
-
-def start_app():
-    with ui.row():
-        login_page()
-    ui.run(title="Gimme your money", port=8080)
