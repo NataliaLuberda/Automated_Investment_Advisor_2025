01539a431e8a307cd1d9afd7a36cd5c68d287891
Natalia <nluberda@student.agh.edu.pl>
2025-05-25 21:17:51 +0200

    logout

--- application/views/home.py
+++ application/views/home.py
@@ -6,5 +6,5 @@ from application.session import logout_user
 @ui.page("/home")
 def home_page():
     ui.label("🏠 Strona Główna").classes("text-h4")
-    ui.button("Wyloguj", on_click=lambda: (logout_user(context.client), ui.navigate.to("/login")))
+    ui.button("Wyloguj", on_click=lambda: (logout_user(), ui.navigate.to("/login")))
     ui.button("Konta", on_click=lambda: ui.navigate.to("/account"))
