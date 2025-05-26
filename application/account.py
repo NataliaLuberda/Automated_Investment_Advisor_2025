42e6457c0b93b02b75fdb072360abd28c1b3a175
Natalia <nluberda@student.agh.edu.pl>
2025-05-26 20:59:33 +0200

    account-edit-delete

--- application/account.py
+++ application/account.py
@@ -29,3 +29,48 @@ def create_account(currency: str, balance: float) -> str:
     db.commit()
     db.close()
     return f"✅ Dodano konto: {currency} ({balance:.2f})"
+
+
+def delete_account(account_id: int):
+    db = SessionLocal()
+    email = get_logged_user_email()
+    user = db.query(User).filter_by(email=email).first()
+
+    if not user:
+        db.close()
+        raise Exception("❌ Użytkownik niezalogowany!")
+
+    account = db.query(Account).filter_by(id=account_id, user_id=user.id).first()
+    if not account:
+        db.close()
+        raise Exception("❌ Konto w danej walucie nie istnieje!")
+
+    if account.balance > 0:
+        db.close()
+        raise Exception("❌ Konto zawiera środki! Przenieś je, a następnie usuń konto.")
+
+    db.delete(account)
+    db.commit()
+    db.close()
+    return f"✅ Usunięto konto: {account.currency}"
+
+
+def update_account_balance(account_id: int, new_balance: float) -> str:
+    db = SessionLocal()
+    email = get_logged_user_email()
+    user = db.query(User).filter_by(email=email).first()
+
+    if not user:
+        db.close()
+        return "❌ Operacja nie powiodła się!"
+
+    account = db.query(Account).filter_by(id=account_id, user_id=user.id).first()
+
+    if not account:
+        db.close()
+        return "❌ Operacja nie powiodła się!"
+
+    account.balance = new_balance
+    db.commit()
+    db.close()
+    return f"✏️ Zaktualizowano saldo konta {account.currency} na {new_balance:.2f}"
