b9559dd496ef7e5edfd836b184d7c1caeb27af60
Natalia <nluberda@student.agh.edu.pl>
2025-06-06 09:29:13 +0200

    add import to run.py

--- run.py
+++ run.py
@@ -1,5 +1,10 @@
 from nicegui import ui
-
+from application.services.database import init_db
+from application.views.login import login_page
+from application.views.accounts import account_page
+from application.views.register import register_page
+from application.views.home import home_page
+from application.views.konto import konto_page
 from application.services.database import init_db
 
 
