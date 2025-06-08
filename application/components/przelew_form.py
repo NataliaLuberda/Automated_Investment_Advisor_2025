bcb908e7a8ae7b40f0c39d0c9090ce8bc3643fdf
nifiz <nifizek@gmail.com>
2025-06-08 18:03:29 +0200

    Porządki w formularzu przelewu.

---
+++ application/components/przelew_form.py
@@ -0,0 +1,103 @@
+from nicegui import ui
+from application.models import Transakcja
+from application.utils.user_info import UserInfo
+from application.cqrs.commands.wyslij_przelew import WyslijPrzelew
+from application.utils.user_info import UserInfo
+
+
+class PrzelewForm:
+    def __init__(self, user_info: UserInfo) -> None:
+
+        with ui.element().classes("w-full flex justify-center"):
+            with ui.element() as self.button_container:
+                self.button_reference = ui.button(
+                    "Nowy przelew",
+                    icon="add",
+                    on_click=lambda: (self.show_form_hide_button()),
+                )
+
+            with ui.element() as self.form_container:
+                self.form_container.visible = False
+                self.form_reference = self.przelew_form(user_info)
+
+        return
+
+    def show_form_hide_button(self):
+        self.button_container.visible = False
+        self.form_container.visible = True
+
+    def hide_form_show_button(self):
+        self.form_container.visible = False
+        self.button_container.visible = True
+
+    def przelew_form(self, user_info: "UserInfo") -> ui.element:
+
+        transakcja = Transakcja()
+
+        with ui.element() as form:
+
+            with ui.column().classes("w-full border-4"):
+                select_options = {
+                    acc.id: f"{acc.currency}" for acc in user_info.account_list
+                }
+
+                ui.select(
+                    options=select_options,
+                    label="Z rachunku:",
+                    value=transakcja.id_sender,
+                ).classes("w-full").bind_value(transakcja, "id_sender")
+
+                ui.number(
+                    label="Kwota przelewu",
+                    value=0,
+                    min=0.01,
+                    max=999999999.99,
+                    step=1.0,
+                ).classes("flex w-full justify-center items-center").bind_value(
+                    transakcja, "amount_numeric"
+                )
+
+                ui.number(label="Numer rachunku adresata").bind_value(
+                    transakcja, "id_receiver"
+                ).classes("w-full")
+
+                ui.textarea(
+                    label="Opis przelewu",
+                    validation={
+                        "Zbyt długi opis": lambda x: len(x or "")
+                        <= transakcja.MAX_DESC_LENGTH
+                    },
+                ).classes("w-full").bind_value(transakcja, "description").props(
+                    f"maxlength={transakcja.MAX_DESC_LENGTH}"
+                )
+
+                with ui.row().classes("w-full flex justify-between"):
+
+                    ui.button(
+                        "Wyślij",
+                        icon="send",
+                        on_click=lambda: (self.wyslij_przelew_onclick(transakcja)),
+                    )
+
+                    ui.button(
+                        "Anuluj",
+                        icon="cancel",
+                        on_click=lambda x: (self.hide_form_show_button()),
+                    ).props("color=red")
+
+        return form
+
+    async def wyslij_przelew_onclick(self, transakcja: Transakcja) -> None:
+        request = WyslijPrzelew.Request(
+            id_nadawcy=transakcja.id_sender,
+            id_adresata=transakcja.id_receiver,
+            kwota=transakcja.amount_numeric,
+            opis=transakcja.description,
+        )
+        try:
+            await WyslijPrzelew.handle(request)
+            ui.notify(f"Wysłano przelew! 🚀", type="positive")
+            self.hide_form_show_button()
+
+        except Exception as e:
+            ui.notify(f"Wysyłka nie powiodła się: {e}", type="negative")
--- application/views/konto.py
+++ application/views/konto.py
@@ -3,7 +3,6 @@ from dataclasses import dataclass
 from nicegui import ui  # type: ignore
 
 from application.account import get_user_accounts
-from application.cqrs.commands.wyslij_przelew import WyslijPrzelew
 from application.cqrs.queries.daj_historie_transakcji import (
     daj_historie_transakcji_uzytkownika,
 )
@@ -12,64 +11,96 @@ from application.models import User, Account
 from application.session import get_logged_user_email
 from application.components.navbar import navbar
 from application.utils.user_info import UserInfo
-from application.components.transfer_form import przelew_form
+from application.components.przelew_form import PrzelewForm
 
 
 @ui.page("/konto")
 def konto_page():
-    uzytkownik: User = get_user_details()
-    rachunki_uzytkownika: list[Account] = get_user_accounts()
-    user_info = UserInfo(uzytkownik, rachunki_uzytkownika)
-    historia_transakcji = daj_historie_transakcji_uzytkownika(uzytkownik.id)
 
     with ui.column().classes("items-center w-full"):
 
         navbar()
 
-        with ui.card().classes(
-            "w-96 min-h-0 max-h-full flex flex-col items-left justify-left"
-        ):
-            with ui.card_section().classes("w-full"):
-                ui.label(uzytkownik.email).classes("text-4xl w-full text-center")
+        try:
 
-            with ui.card_section().classes("w-full h-full"):
+            uzytkownik: User = get_user_details()
+            rachunki_uzytkownika: list[Account] = get_user_accounts()
+            user_info = UserInfo(uzytkownik, rachunki_uzytkownika)
+            historia_transakcji = daj_historie_transakcji_uzytkownika(uzytkownik.id)
 
-                przelew_form(user_info)
+            with ui.card().classes(
+                "w-50 min-h-0 max-h-full flex flex-col items-left justify-left"
+            ):
+                with ui.card_section().classes("w-full"):
+                    ui.label(uzytkownik.email).classes("text-4xl w-full text-center")
 
-            with ui.card_section():
-                with ui.tabs() as tabs:
-                    historia = ui.tab("Historia")
-                    rachunki = ui.tab("Moje rachunki")
-                with ui.tab_panels(tabs, value=historia):
-                    with ui.tab_panel(historia):
-                        ui.label("Historia transakcji")
+                with ui.card_section().classes("w-full h-full"):
 
-                        for t in historia_transakcji:
-                            if t.id_sender == uzytkownik.id:
-                                color = "red"
-                                przychodzacy_wychodzacy = "wychodzący"
-                            else:
-                                color = "green"
-                                przychodzacy_wychodzacy = "przychodzący"
+                    PrzelewForm(user_info)
 
-                            with ui.row().classes(f"text-{color}-500 border-2"):
-                                ui.label(f"Data: {t.timestamp.date()}")
-                                ui.label(
-                                    f"Przelew {przychodzacy_wychodzacy}: {t.amount_numeric}"
+                with ui.card_section():
+                    with ui.tabs() as tabs:
+                        historia = ui.tab("Historia")
+                        rachunki = ui.tab("Moje rachunki")
+                    with ui.tab_panels(tabs, value=historia).classes("w-full"):
+
+                        with ui.tab_panel(historia):
+
+                            with ui.list().props("bordered separator"):
+
+                                ui.item_label("Historia transakcji").props(
+                                    "header"
+                                ).classes("text-bold")
+                                ui.separator()
+
+                                for t in historia_transakcji:
+                                    if t.id_sender == uzytkownik.id:
+                                        color = "red"
+                                        przychodzacy_wychodzacy = "wychodzący"
+                                    else:
+                                        color = "green"
+                                        przychodzacy_wychodzacy = "przychodzący"
+
+                                    with ui.item():
+                                        with ui.row().classes(f"text-{color}-500"):
+                                            ui.item_label(f"Data: {t.timestamp.date()}")
+                                            ui.item_label(
+                                                f"Przelew {przychodzacy_wychodzacy}: {t.amount_numeric}"
+                                            )
+                                            with ui.row().classes(f"text-black"):
+                                                ui.item_label(
+                                                    f"Z rachunku: {t.id_sender}"
+                                                )
+                                                ui.item_label(
+                                                    f"Na rachunek: {t.id_receiver}"
+                                                )
+
+                        with ui.tab_panel(rachunki):
+
+                            with ui.list().props("bordered separator"):
+                                ui.item_label("Moje rachunki").props("header").classes(
+                                    "text-bold"
                                 )
+                                ui.separator()
 
-                    with ui.tab_panel(rachunki):
-                        for rachunek in rachunki_uzytkownika:
-                            with ui.row():
-                                with ui.column().classes("w-full"):
-                                    ui.label(f"Numer konta: {rachunek.id}")
-                                    ui.label(
-                                        f"Saldo: {rachunek.balance} {rachunek.currency}"
-                                    )
-                                    ui.separator()
+                                for rachunek in rachunki_uzytkownika:
+                                    with ui.item():
+                                        with ui.column().classes("w-full"):
+                                            ui.item_label(f"Nr. konta: {rachunek.id}")
+                                            ui.item_label(
+                                                f"Saldo: {rachunek.balance} {rachunek.currency}"
+                                            )
+        except Exception as e:
+            ui.label("Wystąpił błąd. Spróbuj ponownie później.")
+            ui.notify(f"Parametry wyjątku: {e}.", type="negative")
 
 
 def get_user_details() -> "User":
     uemail = get_logged_user_email()
     request = DajUzytkownika.Request(uemail)
-    return DajUzytkownika.handle(request=request)
+    uzytkownik: User = DajUzytkownika.handle(request=request)
+
+    if uzytkownik is None:
+        raise ValueError("Nie znaleziono użytkownika")
+
+    return uzytkownik
