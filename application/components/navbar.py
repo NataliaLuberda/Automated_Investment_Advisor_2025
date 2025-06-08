cd6dfab55d05ad1c517c4495e018615209a873ef
nifiz <nifizek@gmail.com>
2025-06-08 16:27:24 +0200

    Navbar poszedł do osobnego komponentu, żeby nie powtarzać kodu. Zmiany stylizacyjne.

---
+++ application/components/navbar.py
@@ -0,0 +1,22 @@
+from nicegui import ui
+from application.session import logout_user
+
+
+def navbar() -> None:
+
+    with ui.row().classes(
+        "w-full justify-between items-center px-10 py-4 bg-gradient-to-r from-blue-500 to-purple-500 text-white shadow-lg rounded-lg"
+    ) as navbar:
+        ui.button(icon="savings", on_click=lambda: ui.navigate.to("/savings")).props(
+            "flat color=white"
+        )
+        ui.button(
+            icon="account_balance", on_click=lambda: ui.navigate.to("/account")
+        ).props("flat color=white")
+        ui.button(icon="send", on_click=lambda: (ui.navigate.to("/konto"))).props(
+            "flat color=white"
+        ).tooltip("Wyślij przelew")
+        ui.button(
+            icon="logout",
+            on_click=lambda: (logout_user(), ui.navigate.to("/login")),
+        ).props("flat color=white")
